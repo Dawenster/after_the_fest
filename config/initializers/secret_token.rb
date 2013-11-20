21991b714d5bae5c7316280309eab9cfa0895d8d
Dawenster <dstwen@gmail.com>
2013-11-20 09:59:58 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AfterTheFest::Application.config.secret_key_base = '2496db7605828269ffa3ad2b73b6e4e8c7e4c6e29dc6abfc0379605194fd337da536cfc1aba4152578e06c6d8628ca8ee593e049118cc242b61f664d49a14ad2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AfterTheFest::Application.config.session_store :cookie_store, key: '_after_the_fest_session'
