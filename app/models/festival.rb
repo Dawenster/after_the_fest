ea2ba3d9a24c199b06a5d8bf99475a9ba13de854
Dawenster <dstwen@gmail.com>
2013-11-20 16:38:39 -0800

    finished admin manage festival

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,6 +1,6 @@
 class Festival < ActiveRecord::Base
-  has_attached_file :logo, :styles => { :medium => "200x150>", :thumb => "100x100>" }
-  has_attached_file :banner, :styles => { :medium => "700x300>" }
+  has_attached_file :logo, :styles => { :thumb => "100x100>" }
+  has_attached_file :banner, :styles => { :thumb => "300x150>" }
 
   validates :name, :presence => true
   validates :festival_url, :presence => true
