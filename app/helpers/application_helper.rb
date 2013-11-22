9fce4bed8a255e7bfbb163945fd18e5f3ab4ae45
Dawenster <dstwen@gmail.com>
2013-11-21 17:47:03 -0800

    sticky footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def set_video_dimentions(str)
+    str.gsub!("780px", "100%")
+    str.gsub!("440px", "100%")
+    str.gsub!("content=\"780\"", "content=\"100%\"")
+    str.gsub!("content=\"440\"", "content=\"100%\"")
+    return str
+  end
 end
