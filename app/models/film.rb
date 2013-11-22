140a5aca8d6261f1a58bee20102c1bbf5add8635
Dawenster <dstwen@gmail.com>
2013-11-22 15:24:00 -0800

    html style on popover

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,4 +1,6 @@
 class Film < ActiveRecord::Base
+  include ActionView::Helpers::TextHelper
+
   s3_credentials_hash = {
     :access_key_id => ENV['AWS_ACCESS_KEY'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
@@ -22,5 +24,19 @@ class Film < ActiveRecord::Base
 
   def to_slug
     self.name.parameterize
+  end
+
+  def popover_content
+    return content = <<-eos
+      <strong>Directed by:</strong> #{self.director}<br>
+      <strong>Writer:</strong> #{self.writer}<br>
+      <strong>Starring:</strong> #{self.starring}<br>
+      <br>
+      #{truncate(self.description, length: 100, omission: "... ")} #{"<a href=/#{self.festival.slug}/#{self.slug}>more</a>" if self.description.length > 100}<br>
+      <br>
+      <strong>Run time:</strong> #{self.run_time} mins<br>
+      <strong>Screening:</strong> #{self.screening}<br>
+      <strong>Available:</strong> #{self.start.strftime('%b')} #{self.start.day.ordinalize} to #{self.end.strftime('%b')} #{self.end.day.ordinalize}
+    eos
   end
 end
