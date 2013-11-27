bf425fbc67f486bafc928f6b7798e47d8a2bc1dc
Dawenster <dstwen@gmail.com>
2013-11-26 22:49:35 -0800

    comments posting immediately with correct captcha errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,9 +8,13 @@ class CommentsController < ApplicationController
       params["comment"]["film_id"] = params["comment"]["film_id"].to_i
       comment = Comment.new(params["comment"])
       if simple_captcha_valid? && comment.save
-        format.json { render :json => { :message => "Success!" }, :status => :ok }
+        results = {
+          :comment => render_to_string(:partial => "individual_comment.html.slim", :locals => { :comment => comment }),
+          :captcha => render_to_string(:partial => "captcha.html.slim")
+        }
+        format.json { render :json => results }
       else
-        format.json { render :json => { :message => comment.errors.messages }, :status => :unprocessable_entity }
+        format.json { render :json => { :message => "Something went wrong - please try again." }, :status => :unprocessable_entity }
       end
     end
   end
