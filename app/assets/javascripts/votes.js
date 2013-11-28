74e72a4bc0604338a09c376fe8d45e0fed6cc358
Dawenster <dstwen@gmail.com>
2013-11-28 15:04:30 -0800

    fix voting ajax

---
+++ app/assets/javascripts/votes.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $(".vote-button").click(function(e) {
+    $.ajax({
+      url: '/votes',
+      method: 'post',
+      dataType: 'json',
+      data: { film_id: $(this).attr("data-film-id"), vote_type: $(this).attr("data-vote-type") }
+    })
+    .done(function(data) {
+      var count = "  " + data.count;
+      if (data.vote_type == "up") {
+        $(".fa-thumbs-up").text(count);
+      } else {
+        $(".fa-thumbs-down").text(count);
+      }
+      $(".voting-space").text("Thank you for voting");
+    })
+  });
+});
